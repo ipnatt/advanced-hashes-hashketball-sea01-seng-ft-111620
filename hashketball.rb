00196a52ef37fc5071ac9fddab8de2c57709e70f
Irvin Natt <ipnatt@gmail.com>
2020-11-10 00:59:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,16 +166,13 @@ def team_names
 end
 
 def player_numbers(team_name)
-  numbers = []
   game_hash.each do |player, player_number|
     if player_number[:team_name] == team_name
       return (player_number[:player_name].map do |players, player_count|
         player_count[:team_name]
-        numbers = player_count[:team_name]
       end
     end
   end
-  numbers
 end
 
 
