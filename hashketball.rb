6f4665faf616aa452c649ddc4738d6fda6e5cc08
Irvin Natt <ipnatt@gmail.com>
2020-11-10 01:38:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ def player_stats(player_name)
   stats = {}
   game_hash.each do |player, player_stat|
     player_stat[:players].each do |statistics|
-    if statistics[:player_name] == player_name
+      if statistics[:player_name] == player_name
       player_stat = statistics
     end
   end
